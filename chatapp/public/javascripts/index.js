b405e4f755d15704cbc5367535def30605f881de
takaya468 <takaya07161995@gmail.com>
2019-04-10 19:36:08 +0900

    committed in 2019/04/10 19:36:08

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// チャットルームに入室する
-function enter() {
-    // 入力されたユーザ名を取得する
-    const userName = '';
-    // ユーザ名が未入力でないかチェックする
+	// チャットルームに入室する
+	function enter() {
+	    // 入力されたユーザ名を取得する
+	    const userName = '';
+	    // ユーザ名が未入力でないかチェックする
 
 
-    $('form').submit();
-}
+	    $('form').submit();
+	}
