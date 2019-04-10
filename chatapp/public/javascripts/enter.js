6578a5952f4369c23501192e0ef81f4b3d473a33
tatsuro2019 <zakipit@yahoo.co.jp>
2019-04-10 20:42:37 +0900

    committed in 2019/04/10 20:42:37

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,5 +1,6 @@
 'use strict';
 
+<<<<<<< Updated upstream
 	// 入室メッセージをサーバに送信する
 	// 入力されたユーザ名を取得する
 	const userName = '';
@@ -10,3 +11,24 @@
 	socket.on('', function (data) {
 	    $('#thread').prepend('<p>' + '</p>');
 	});
+=======
+const socket =io.connect();
+// 入室メッセージをサーバに送信する
+socket.on('enterMyselfEvent',function(data){
+  $('#thread').prepend('<p>' + data + '</p>');
+});
+
+socket.on('sendOtherEvent',function(data){
+  $('#thread').prepend('<p>' + data + '</p>');
+});
+// 入力されたユーザ名を取得する
+const userName = 'hhhh';
+// 入室メッセージイベントを送信する
+socket.emit('sendMessageEvent',userName);
+
+
+// サーバから受信した入室メッセージを画面上に表示する
+socket.on('receiveMessageEvent', function (data) {
+    $('#thread').prepend('<p>' + data + '</p>');
+});
+>>>>>>> Stashed changes
