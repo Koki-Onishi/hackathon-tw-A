26c10b9983ad80b0c9e3ab7cb4a7091fc1a36964
takaya468 <takaya07161995@gmail.com>
2019-04-10 20:02:32 +0900

    committed in 2019/04/10 20:02:32

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -5,8 +5,8 @@
 	    // 入力されたユーザ名を取得する
 	    const userName = $('#userName').text();
 	    // ユーザ名が未入力でないかチェックする
-			if (userName !== ''){
+			//if (userName !== ''){
 				$('form').submit();
-			}
+			//}
 
 	}
