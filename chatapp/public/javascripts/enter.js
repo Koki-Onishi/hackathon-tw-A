bea6e149a8d0ea3c6c2b2302749cfb7b47cf12c0
tatsuro2019 <zakipit@yahoo.co.jp>
2019-04-11 04:14:45 +0900

    committed in 2019/04/11 04:14:45

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,34 +1,15 @@
 'use strict';
 
-<<<<<<< Updated upstream
-	// 入室メッセージをサーバに送信する
-	// 入力されたユーザ名を取得する
-	const userName = '';
-	// 入室メッセージイベントを送信する
+const socket = io.connect();
 
-
-	// サーバから受信した入室メッセージを画面上に表示する
-	socket.on('', function (data) {
-	    $('#thread').prepend('<p>' + '</p>');
-	});
-=======
-const socket =io.connect();
-// 入室メッセージをサーバに送信する
-socket.on('enterMyselfEvent',function(data){
-  $('#thread').prepend('<p>' + data + '</p>');
-});
-
-socket.on('sendOtherEvent',function(data){
-  $('#thread').prepend('<p>' + data + '</p>');
-});
-// 入力されたユーザ名を取得する
-const userName = 'hhhh';
-// 入室メッセージイベントを送信する
-socket.emit('sendMessageEvent',userName);
-
-
-// サーバから受信した入室メッセージを画面上に表示する
-socket.on('receiveMessageEvent', function (data) {
+// 自クライアントの接続イベント（enterMyselfEvent）を受信する
+socket.on('enterMyselfEvent', function (data) {
+    // 画面上にメッセージを表示
     $('#thread').prepend('<p>' + data + '</p>');
 });
->>>>>>> Stashed changes
+
+// 自クライアント以外の接続イベント（enterOtherEvent）を受信する
+socket.on('enterOtherEvent', function (data) {
+    // 画面上にメッセージを表示
+    $('#thread').prepend('<p>' + data + '</p>');
+});
