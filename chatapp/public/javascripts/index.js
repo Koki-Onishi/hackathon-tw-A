2ea75f935f0f05131108e928bed1a7d23a722df8
takaya468 <takaya07161995@gmail.com>
2019-04-10 19:58:06 +0900

    committed in 2019/04/10 19:58:06

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,8 @@
 	// チャットルームに入室する
 	function enter() {
 	    // 入力されたユーザ名を取得する
-	    const userName = '';
+	    const userName = $('userName').text();
 	    // ユーザ名が未入力でないかチェックする
-			const userName = $('userName').text('');
 			if (userName !== ''){
 				$('form').submit();
 			}
