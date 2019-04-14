d375aaecdba175f5e9e80024181fecdd648b8a02
tatsuro2019 <49042734+tatsuro2019@users.noreply.github.com>
2019-04-15 08:58:15 +0900

    Update enter.js

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,14 +2,15 @@
 
 module.exports = function (socket) {
   // 入室メッセージをクライアントに送信する
-  socket.on('sendEnterEvent', function (data) {
+  socket.emit('enterEventstart','');
+  socket.on('enterEventsecond', function (data) {
       if (!data) {
           return;
       }
 
       console.log('入室：' + data);
 
-      // 他クライアントが受信するメッセージ表示イベント（receiveMessageEvent）を送信する
-      socket.broadcast.emit('receiveMessageEvent', data);
+      // 全クライアントが受信するメッセージ表示イベント（receiveMessageEvent）を送信する
+      socket.broadcast.emit('enterEventfinal', data);
   });
 };
