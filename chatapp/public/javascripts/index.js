8fa84a9c293597a733d48497912f58c09442a46a
tatsuro2019 <49042734+tatsuro2019@users.noreply.github.com>
2019-04-15 09:29:08 +0900

    Update index.js

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,14 +3,13 @@
 	// チャットルームに入室する
 	function enter() {
 	    // 入力されたユーザ名を取得する
-
 	    const userName = $('#userName').val();
-			//alert(userName);
+			alert(userName);
 	    // ユーザ名が未入力でないかチェックする
 			if(userName !== ''){
-				alert('ユーザー名: '+ userName + ' でログインします');
+				alert('ようこそ');
 				$('form').submit();
-			}elseif(userName === '' || userName === '/'){
+			}else{
 				alert('文字を入力してください');
 		}
 	}
