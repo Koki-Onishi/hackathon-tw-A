9d5cb3a8f902b7a3ffe49b3a313472cd359be2b0
takaya468 <takaya07161995@gmail.com>
2019-04-17 20:52:02 +0900

    committed in 2019/04/17 20:52:02

--- chatapp/public/javascripts/reverse_thread.js
+++ chatapp/public/javascripts/reverse_thread.js
@@ -1,4 +1,4 @@
 function reverse_thread() {
-  thread = $('#thread');
-  thread.children().prepend(thread.children().each(function (i, p) { thread.prepend(p) }));
-}
+	  thread = $('#thread');
+	  thread.children().prepend(thread.children().each(function (i, p) { thread.prepend(p) }));
+	}
